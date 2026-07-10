8de5d01bd9f3dc0a2032c49f5c87fe8e639fa1a9
Sebastian Franco <jhonsfran@gmail.com>
2026-07-10 19:04:55 +0200

    chore: cnfast

--- internal/ui/src/utils/index.ts
+++ internal/ui/src/utils/index.ts
@@ -1,10 +1,4 @@
-import { clsx } from "clsx"
-import type { ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+export { cn } from "cnfast"
 
 export const focusRing = [
   // base
